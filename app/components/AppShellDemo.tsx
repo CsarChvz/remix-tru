df76c7166f14bf3838f60720bab5e9fb80d87811
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2022-10-02 19:38:38 -0500

    Supabase addition -- auth-helpers failed
    https://github.com/supabase/supabase-js/issues/526 -- Failed
    
    https://supabase.com/docs/guides/with-nextjs

--- app/components/AppShellDemo.tsx
+++ app/components/AppShellDemo.tsx
@@ -10,6 +10,7 @@ import {
   Burger,
   useMantineTheme,
 } from "@mantine/core";
+import { NavbarMinimal } from "./Links";
 
 export default function AppShellDemo({
   children,
@@ -30,6 +31,16 @@ export default function AppShellDemo({
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
       header={
         <Header height={70} p="md">
           <div
